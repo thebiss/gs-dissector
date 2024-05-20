eae3f3975662e50cf31b7734304d48e9a6f8244a
thebiss <bbissell@gmail.com>
2024-05-19 22:08:50 -0400

    fix: don't filter [CRC:BAD] from oncor data, seethe  25Mar2023 video

--- sample-cap/Oncor-txt-to-hd-to-pcap.sh
+++ sample-cap/Oncor-txt-to-hd-to-pcap.sh
@@ -5,10 +5,11 @@ basefile=$(basename -a "${srcfile}")
 hdfile="${basefile}.hd"
 pcapfile="${basefile}.pcap"
 
+### This used to remove CRC BAD, but they seem to include other examples...
+# | grep -v "^\[CRC:BAD\]" \
 echo "Creating HD file..."
 cat ${srcfile} \
-	| grep -v "^\[CRC:BAD\]" \
-	| sed -E 's/\[CRC: OK\]\s*//' \
+	| sed -E 's/\[CRC:...\]\s*//' \
 	| sed -E 's/\w*Baudrate.*$//' \
 	| sed 's/\(\w\w\)/ \1/g' \
 	| sed 's/^\(.*\)/000000\t\1\n/' > "${hdfile}"
