fdef970a20379c9f4daf6ff9d893f1a522a5f8ab
thebiss <bbissell@gmail.com>
2024-08-04 20:07:00 -0400

    fix: support wireshark 4.3 windows plugin folder

--- promote.sh
+++ promote.sh
@@ -1,3 +1,5 @@
 #!
 echo "Copying to the wireshark plugin directory..."
 cp -v gridstream.lua ~/Downloads/WiresharkPortable64/App/Wireshark/plugins/4.2/epan
+cp -v gridstream.lua ~/Downloads/WiresharkPortable64/App/Wireshark/plugins/4.3/epan
+
